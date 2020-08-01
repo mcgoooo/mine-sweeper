ca799dbf3f9224e29e44a1b9a9ec3533ca5d2c9f
Andrew Hilton <igberty@gmail.com>
2020-08-01 14:02:14 +0100

    feat: add right click handler + test interactions
    The implementation now uses two different reducer(esque) patterns
    to deal with user interactions. Due to games inherently having a
    lot of shared state, sometimes you have to interact with most of
    the state at once. I have dealt with this conundrum before, and
    it's a tricky one.
    
    This is why i resort to using cloneDeep, and accepting some mutating
    of state in the reducers. In production, you may wish to use something
    like immutable.js or a similar pattern, but this is enough for a mvp.
    it's mainly about the readability for me in a code test,
    this is one of the  parts i was not entirely happy with.
    
    The longest part of this individual feature was the testing, but I am
    happy with the level of coverage that it now has.

---
+++ tests/mocks/game.js
@@ -0,0 +1,83 @@
+import cloneDeep from 'lodash.clonedeep'
+import { bombType } from '../../models/game/generator'
+import { fillInSquares }  from '../../models/game/generator'
+
+const bm = () => ({...bombType})
+
+const threeByThreeBoard = [
+  [bm(),null,null],
+  [null,null,null],
+  [bm(),null,null]
+]
+const surrondedByBombsBoard = [
+  [bm(),bm(),bm()],
+  [bm(),null,bm()],
+  [bm(),bm(),bm()]
+]
+
+
+const blockingWallBoard = [
+  [bm(),null,null,null,null,null,null],
+  [bm(),null,null,null,bm(),null,null],
+  [bm(),null,null,null,bm(),null,null],
+  [bm(),null,null,null,bm(),null,null],
+  [bm(),null,null,null,bm(),null,null],
+  [bm(),null,null,null,bm(),null,null],
+  [bm(),null,null,null,bm(),null,null],
+]
+
+const nonBlockingWallBoard = [
+  [bm(),null,null,null,null,null,null],
+  [bm(),null,null,null,null,null,null],
+  [bm(),null,null,null,bm(),null,null],
+  [bm(),null,null,null,bm(),null,null],
+  [bm(),null,null,null,bm(),null,null],
+  [bm(),null,null,null,bm(),null,null],
+  [bm(),null,null,null,bm(),null,null],
+]
+
+const generate = (originalBoard, shouldFillInSquares = true) => {
+  const board = shouldFillInSquares ?
+    fillInSquares(cloneDeep(originalBoard)): cloneDeep(originalBoard)
+  const locations = generateLocations(board)
+  const bombCount = board.flat().filter((square)=>square.type == 'bomb').length
+  return {
+    game: {
+      board: board,
+      status: 'started',
+      bombCount: bombCount
+    },
+    locations
+  }
+}
+
+
+const generateLocations = (board) => {
+  const locations = {
+    bomb: [],
+    emptySquare: [],
+    bombNearby: []
+  }
+
+  board.forEach((row, rowIndex)=> {
+    row.forEach((square, squareIndex)=> {
+      locations[square.type].push({rowIndex, squareIndex})
+    })
+  })
+
+  return locations
+}
+
+
+export const threeByThree = (fillsquares) =>
+  generate(threeByThreeBoard, fillsquares)
+
+export const blockingWall = (fillsquares) =>
+  generate(blockingWallBoard, fillsquares)
+
+export const nonBlockingWall = (fillsquares) =>
+  generate(nonBlockingWallBoard, fillsquares)
+
+export const surroundedByBombs = (fillsquares) =>
+  generate(surrondedByBombsBoard, fillsquares)
+
--- tests/pages/index.test.js
+++ tests/pages/index.test.js
@@ -1,34 +1,66 @@
-import TestRenderer from 'react-test-renderer'
+import TestRenderer, { act } from 'react-test-renderer'
 import React from 'react'
 import Index, { getStaticProps } from '../../pages/index'
-import Mine from '../../components/mine';
 import Square from '../../components/square';
-
 
 // TODO
 global.React = React
 
+const getSquareType =
+  (squares, type) => squares.filter((square) => square.props.type == type)
+
 describe('index', () => {
-  const props = {...getStaticProps().props}
-  const rendered = TestRenderer.create(<Index { ...props}/>)
-  const squares = rendered.root.findAllByType(Square)
+  describe('initial render',()=>{
+    const props = {...getStaticProps().props}
+    const rendered = TestRenderer.create(<Index { ...props}/>)
+    const root = rendered.root
+    const squares = root.findAllByType(Square)
 
-  it('renders the correct title', () => {
-    const title = rendered.toTree().rendered.props.title
-    expect(title).toBe("Minesweeper (active)")
+    it('renders the correct title', () => {
+      const title = rendered.toTree().rendered.props.title
+      expect(title).toBe("Minesweeper (active)")
+    })
+
+    it('renders the correct amount of bombs', () => {
+      expect(getSquareType(squares, 'bomb').length).toBe(10)
+    })
+
+    it('renders the rest of the squares', () => {
+      const empty = getSquareType(squares, 'emptySquare').length
+      const nearby = getSquareType(squares, 'bombNearby').length
+      expect(empty > 10).toBe(true)
+      expect(nearby > 10).toBe(true)
+      expect(empty + nearby).toBe(90)
+    })
   })
 
-  it('renders the correct amount of bombs', () => {
-    expect(rendered.root.findAllByType(Mine).length).toBe(10)
+
+  describe('play a game',()=>{
+    it('can win a game',()=>{
+      const props = {...getStaticProps().props}
+      const rendered = TestRenderer.create(<Index { ...props}/>)
+      const root = rendered.root
+      const squares = root.findAllByType(Square)
+
+      const bombs = getSquareType(squares, 'bomb')
+      bombs.forEach((bomb)=> act(()=> {
+        bomb.props.onContextMenu({ preventDefault:()=>{} })
+      }))
+      const haveWon = root.findAllByProps({status: 'won'})
+      expect(haveWon.length > 0).toBe(true)
+    })
+
+    it('can lose a game',()=>{
+      const props = {...getStaticProps().props}
+      const rendered = TestRenderer.create(<Index { ...props}/>)
+      const root = rendered.root
+      const squares = root.findAllByType(Square)
+      const bomb = getSquareType(squares, 'bomb')[0]
+
+      act(bomb.props.onClick)
+      const haveLost = root.findAllByProps({status: 'lost'})
+      expect(haveLost.length > 0).toBe(true)
+    })
   })
 
-  it('renders some empty squares', () => {
-    const emptySquares = squares.filter((square) => square.props.type == "emptySquare")
-    expect(emptySquares.length > 10).toBe(true)
-  })
-
-  it('renders some BombNearby squares', () => {
-    const BombNearby = squares.filter((square) => square.props.type == "bombNearby")
-    expect(BombNearby.length > 10).toBe(true)
-  })
 })
