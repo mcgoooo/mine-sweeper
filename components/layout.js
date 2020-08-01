4b9ce4cc02af42b86f0526727b0b0eb5bbaf61b1
Andrew Hilton <igberty@gmail.com>
2020-08-01 09:01:01 +0100

    chore: move title again
    it already seems to be on layout, i think that may of been a hint
    that i missed, but i have tackled this already, and also have a
    test for it

--- components/layout.js
+++ components/layout.js
@@ -15,6 +15,9 @@ export default ({ children, title = 'Minesweeper' }) => (
         name="viewport"
         content="initial-scale=1.0, width=device-width"
       />
+      <Head>
+      <title>{title}</title>
+    </Head>
     </Head>
     <StyleProvider>
       <Center>
