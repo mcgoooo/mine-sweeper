84a79f47f240a62df3e4c856a6dbe38159d10027
Andrew Hilton <igberty@gmail.com>
2020-08-03 12:02:48 +0100

    chore: create infor bar container
    there was a mix of logic and presentation, so seperate the two.
    i was having trouble tesitng the timers, and didnt want to go
    down a rabbit hole for a code test, so left it.
    
    I also didnt seperate out the container logic for the index page
    there is a fair amount of testing on that already, and it's simple
    enough, just acknowledging that i did think about it, but you have
    to stop polishing somewhere.

---
+++ tests/containers/infoBar.test.js
@@ -0,0 +1,71 @@
+import TestRenderer, { act } from 'react-test-renderer'
+import React from 'react'
+import InfoBarContainer from '../../containers/infoBar'
+import InfoBarComponent from '../../components/infoBar'
+
+// TODO
+global.React = React
+
+describe('info bar container', () => {
+
+  it('renders the infoBarComponent and has the correct status text (start) 😀', () => {
+    const rendered = TestRenderer.create(<InfoBarContainer status={'started'}/>)
+    const root = rendered.root
+    const component = root.findByType(InfoBarComponent)
+
+    expect(component).not.toBeNull()
+    expect(component.props).toEqual({
+      "status": "started",
+      "statusText": "😀",
+      "timeElapsed": 0
+    })
+  })
+
+  it('renders the infoBarComponent and has the correct status text (won) 😎', () => {
+    const rendered = TestRenderer.create(<InfoBarContainer status={'won'}/>)
+    const root = rendered.root
+    const component = root.findByType(InfoBarComponent)
+
+    expect(component).not.toBeNull()
+    expect(component.props).toEqual({
+      "status": "won",
+      "statusText": "😎",
+      "timeElapsed": 0
+    })
+
+  })
+
+  it('renders the infoBarComponent and has the correct status text (loss) 🤬', () => {
+    const rendered = TestRenderer.create(<InfoBarContainer status={'lost'}/>)
+    const root = rendered.root
+    const component = root.findByType(InfoBarComponent)
+
+    expect(component).not.toBeNull()
+    expect(component.props).toEqual({
+      "status": "lost",
+      "statusText": "🤬",
+      "timeElapsed": 0
+    })
+  })
+
+  describe('after one second',()=>{
+    // timers are hard, gonna let this one slide 🤷🏻‍♂️
+    it.skip('advancing the timer (react test render, and jest timers are blocking)',()=>{
+      jest.useFakeTimers()
+      const rendered = TestRenderer.create(<InfoBarContainer status={'lost'}/>)
+      const root = rendered.root
+      act(()=>{
+        jest.advanceTimersByTime(5000);
+        jest.runAllTimers();
+      })
+      rendered.update(<InfoBarContainer status={'lost'}/>)
+      const component = root.findByType(InfoBarComponent)
+      expect(component).not.toBeNull()
+      expect(component.props).toEqual({
+        "status": "lost",
+        "statusText": "🤬",
+        "timeElapsed": 5
+      })
+    })
+  })
+})
