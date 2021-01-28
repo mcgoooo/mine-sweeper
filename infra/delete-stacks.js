90ce334a45c2a3d8c7835d594b99996ff2e7fcdf
Andrew Hilton <igberty@gmail.com>
2021-01-28 22:15:15 +0000

    feat: add in circleci & fargate + fix jest babel
    It is currently a total mvp, until we get the correct routing in for
    production. It also rebuilds on every commit, it's a personal project,
    so fine :D
    
    Also add the ramdisk which is a really useful feature.
    https://circleci.com/blog/the-issue-of-speed-and-determinism-in-ci/

---
+++ infra/delete-stacks.js
@@ -0,0 +1,45 @@
+const AWS = require('aws-sdk')
+
+var cloudformation = new AWS.CloudFormation({region: 'us-east-1'});
+
+
+console.log(cloudformation)
+cloudformation.describeStacks({}, function(err, data) {
+  if (err) {
+    console.log(err);
+    process.exit(1)
+  } else {
+    const stacks = data.Stacks
+    const reviewApps = (stacks.filter((stack)=>
+      stack.Tags &&
+      stack.Tags.length != 0 &&
+      stack.Tags.filter(
+        (tag)=> tag.Key == 'review-environment' && tag.Value == 'true'
+      )
+    ))
+    console.log('REVIEW APPS TO BE DELETED')
+    console.log(reviewApps)
+    console.log('REVIEW APPS WILL BE DELETED in twenty seconds')
+
+      reviewApps.forEach((ra)=>{
+        cloudformation.deleteStack({StackName: ra.StackName},(err, data)=>{
+          if (err) {
+            console.log(err);
+            process.exit(1)
+          } else {
+            console.log(data)
+          }
+        })
+      })
+
+  }
+});
+
+(async () => {
+  try {
+      var text = await main();
+      console.log(text);
+  } catch (e) {
+      // Deal with the fact the chain failed
+  }
+})();
