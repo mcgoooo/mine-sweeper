9204415781cd182eb6b15eca7fc73a1775edcdf6
Andrew Hilton <igberty@gmail.com>
2021-01-28 23:18:28 +0000

    chore: more babel, i hate babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  presets: [
-    ['next/babel', { 'preset-react': { importSource: '@emotion/core' } }],
-  ],
-  plugins: ['emotion'],
+  test: {
+    presets: [
+      ['next/babel', { 'preset-react': { importSource: '@emotion/core' } }],
+    ],
+    plugins: ['emotion'],
+  },
+  "development": {
+    "presets": ["next/babel"]
+  },
+  "production": {
+    "presets": ["next/babel"]
+  }
 };
