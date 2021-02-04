2b26d85773ddc2faa35b7d90a7145c4ce71320d7
Andrew Hilton <igberty@gmail.com>
2021-02-04 13:58:54 +0000

    ferat: add prettier to circleci

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   env: {
     development: {
-      presets: ['next/babel']
+      presets: ["next/babel"],
     },
     production: {
-      presets: ['next/babel']
+      presets: ["next/babel"],
     },
     test: {
-      presets: ['@babel/preset-env', '@babel/react'],
-      plugins: ['@babel/plugin-syntax-jsx']
-    }
-  }
-};
+      presets: ["@babel/preset-env", "@babel/react"],
+      plugins: ["@babel/plugin-syntax-jsx"],
+    },
+  },
+}
