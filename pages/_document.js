6f1e050bddfe8cecd8a14b29910cb6eb70f1dd74
Andrew Hilton <igberty@gmail.com>
2021-02-05 12:20:56 +0000

    chore: move attribution to a componenent (#36)
    fixing tech debt :D

--- pages/_document.js
+++ pages/_document.js
@@ -1,39 +1,6 @@
 import Document, { Head, Main, NextScript } from "next/document"
-import styled from "@emotion/styled"
-const Attirbution = styled.div`
-  padding: 20px;
-  color: white;
-  font-size: 24px;
-  text-align: right;
-  overflow: hidden;
-  position: fixed;
-  right: 0;
-  top: 0;
-  background-color: rgb(60, 170, 170);
-  border-color: rgba(60, 170, 170, 0);
-  border-width: 0 0 6px 6px;
-  border-style: solid;
-  transition: border-color 0.3s;
-  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
-    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
-  &:hover {
-    z-index: 9999;
-    border-color: rgba(70, 190, 190, 1);
-  }
-  a {
-    color: white;
-    display: block;
-    padding: 8px;
-    display: block;
-    text-align: right;
-    &:hover {
-      background: rgba(70, 190, 190, 1);
-    }
-  }
-  a:first-of-type {
-    margin-top: 24px;
-  }
-`
+import Attribution from "../components/attribution"
+
 export default class MyDocument extends Document {
   render() {
     return (
@@ -63,21 +30,21 @@ export default class MyDocument extends Document {
           ></link>
         </Head>
         <body>
-          <Attirbution>
+          <Attribution>
             Minesweeper 95 made by Mcgoooo 🚀
             <a href="https://github.com/mcgoooo/mine-sweeper" target="_blank">
-              source code
+              Source code
             </a>
             <a href="https://github.com/mcgoooo" target="_blank">
               Github
             </a>
             <a href="https://linkedin.com/in/mcgoooo" target="_blank">
-              linked in
+              LinkedIn
             </a>
             <a href="https://instagram.com/mcgoooo" target="_blank">
-              instagram
+              Instagram
             </a>
-          </Attirbution>
+          </Attribution>
           <Main />
           <NextScript />
         </body>
